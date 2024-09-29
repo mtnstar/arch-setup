853ec26062f51df265f9475c10f67c504a908adb
Mountain Star <github@mtnstar.net>
2024-09-29 19:58:48 +0200

    Start adjusting ansible setup for arch

---
+++ ansible/apply.sh
@@ -0,0 +1,9 @@
+sudo ansible-galaxy install -r requirements.yml
+
+if [ $# -eq 0 ]; then
+  sudo ansible-playbook desktop.yml --extra-vars "main_user=$USER"
+else
+  sudo ansible-playbook desktop.yml -t=$1 --extra-vars "main_user=$USER"
+fi
+
+exit 0
