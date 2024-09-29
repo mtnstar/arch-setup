383f8aaa171ffb80322b258101f2ef3b9d9589e9
Mountain Star <github@mtnstar.net>
2024-09-29 16:15:44 +0200

    Install git by default

--- arch-install.sh
+++ arch-install.sh
@@ -201,7 +201,7 @@ microcode_detector
 
 # Pacstrap (setting up a base sytem onto the new root).
 info_print "Installing the base system (it may take a while)."
-pacstrap -K /mnt base linux "$microcode" linux-firmware linux-headers sbctl btrfs-progs grub grub-btrfs rsync efibootmgr snapper reflector snap-pac zram-generator sudo ansible &>/dev/null
+pacstrap -K /mnt base linux "$microcode" linux-firmware linux-headers sbctl btrfs-progs grub grub-btrfs rsync efibootmgr snapper reflector snap-pac zram-generator sudo ansible git &>/dev/null
 
 # Setting up the hostname.
 echo "$hostname" > /mnt/etc/hostname
