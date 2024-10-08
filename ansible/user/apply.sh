83aef991b6a3c8e83812ab7b59293c5a1c0b1b08
Mountain Star <github@mtnstar.net>
2024-10-08 20:22:30 +0200

    Add user git to checkout used git repos

--- ansible/user/apply.sh
+++ ansible/user/apply.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ansible-playbook user.yml --extra-vars "main_user=$USER"
+if [ $# -eq 0 ]; then
+  ansible-playbook user.yml --extra-vars "main_user=$USER"
+else
+  ansible-playbook user.yml -t=$1 --extra-vars "main_user=$USER"
+fi
