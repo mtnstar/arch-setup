54e1c6be98e897373ef07d6645b3c26193880fc7
Mountain Star <github@mtnstar.net>
2024-11-02 07:17:07 +0100

    Add android, add video players

--- ansible/system/apply.sh
+++ ansible/system/apply.sh
@@ -1,6 +1,5 @@
-sudo ansible-galaxy install -r requirements.yml
-
 if [ $# -eq 0 ]; then
+  sudo ansible-galaxy install -r requirements.yml
   sudo ansible-playbook system.yml --extra-vars "main_user=$USER"
 else
   sudo ansible-playbook system.yml -t=$1 --extra-vars "main_user=$USER"
